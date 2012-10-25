61f2fc20d1fe08a69edc4ad8c0ab54f1ab1022ce
Matt Connolly <matt.connolly@me.com>
2012-10-25 21:26:30 +1000

    Rollback changes to util.h

--- re2/util/util.h
+++ re2/util/util.h
@@ -40,19 +40,17 @@ using std::sort;
 using std::swap;
 using std::make_pair;
 
-//#if defined(__GNUC__) && !defined(USE_CXX0X)
-//
-//#include <tr1/unordered_set>
-//using std::tr1::unordered_set;
-//
-//#else
-//
-//#include <unordered_set>
-//using std::unordered_set;
-//#endif
+#if defined(__GNUC__) && !defined(USE_CXX0X)
 
 #include <tr1/unordered_set>
 using std::tr1::unordered_set;
+
+#else
+
+#include <unordered_set>
+using std::unordered_set;
+
+#endif
 
 namespace re2 {
 
